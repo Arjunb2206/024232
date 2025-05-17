fb3fccd780c3788cce9aa52b14e68b54c2d5819e
arjunb2206 <arjun.b@s.amity.edu>
2025-05-17 16:27:58 +0530

    change of all c |hello.c

--- hello.c
+++ hello.c
@@ -1,15 +1,15 @@
 // Sun of two nos `in C Program
 #include <stdio.h>
-
+//program done ::wq
 int main() {
     int p, q, r;
-    
+    //enter values
     printf("Enter two integer values::\n\n");
     scanf("%d %d", &p, &q);
 
     // calculating sum
     r = p - q;      
-    
+   //print it all  
     printf("Result:: %d - %d = %d\n", p, q, r);
     return 0;
 }
